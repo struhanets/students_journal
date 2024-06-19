000ab6c53640fa3431afb663a2bfa9410a3bc996
struhanets <posseydon87@gmail.com>
2024-06-19 12:36:46 +0200

    made migrations, models have been registered in admin.py

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from students.models import Student, Teacher, Group, Subject
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Teacher)
+
+admin.site.register(Group)
+admin.site.register(Subject)
