14c66983f1440417d86a0c00d9360a7faba5e5f6
struhanets <posseydon87@gmail.com>
2024-06-21 09:58:39 +0200

    Added a new folder 'template', created path, view and template for main page

---
+++ students/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from students.views import index
+
+urlpatterns = [
+    path("", index, name="index"),
+]
+
+app_name = "students"
+
--- students/views.py
+++ students/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from students.models import Student, Subject, Teacher
+
+
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+
+    students = Student.objects.all()
+    subjects = Subject.objects.all()
+    teachers = Teacher.objects.all()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        'students': students,
+        'subjects': subjects,
+        'teachers': teachers,
+        'num_visits': num_visits + 1,
+    }
+    return render(request, "students/index.html", context=context)
