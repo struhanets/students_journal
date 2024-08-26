7bd99398ed905bcd0c52586e9ce736908738711b
struhanets <posseydon87@gmail.com>
2024-08-26 10:20:15 +0200

    modify address in allowed_hosts

--- students_journal/settings.py
+++ students_journal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://students-journal.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "students-journal.onrender.com"]
 
 # Application definition
 
